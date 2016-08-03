bc48f1d4d859fb0fc7670fe4d3ad44ad9ae3b52e
Michael <mtevans90@gmail.com>
2016-08-03 11:09:08 -0700

    made safari compatible

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ module.exports = {
   output: {
   	filename: "./lib/bundle.js"
   },
+  module: {
+  loaders: [
+    {
+      test: [/\.js?$/],
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }
+  ]
+},
   devtool: 'source-map',
   resolve: {
     extensions: ["",".js"]
